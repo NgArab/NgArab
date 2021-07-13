66d79849b4ca5f2755e9408d7726644d8f3c2127
Omar Hasan <omar.hassan3030@gmail.com>
2021-07-13 18:23:21 +0200

    feat(user): add red circle in about

--- apps/user/src/app/app.routing.module.ts
+++ apps/user/src/app/app.routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '@core/home/home.component';
 import { AboutComponent } from '@core/about/about.component';
+import { HistoryComponent } from '@core/history/history.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'history', component: HistoryComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
