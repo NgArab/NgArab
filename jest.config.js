1c4779e78df5f71179a8168b18eca37145fff678
Omar Hasan <omar.hassan3030@gmail.com>
2021-09-19 09:00:18 +0300

    fix(global): update packages to latest

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 module.exports = {
-  projects: ['<rootDir>/apps/user', '<rootDir>/apps/admin'],
+  projects: [
+    ...getJestProjects(),
+    '<rootDir>/apps/user',
+    '<rootDir>/apps/admin',
+  ],
 };
