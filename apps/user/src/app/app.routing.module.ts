303590f1c336a5e473fdcb15a851caa5ea3ed73a
Abdelrahman <abdelrahman.sherwida@gmail.com>
2021-10-03 09:38:17 +0200

    fix(home): fix rebase conflicts

--- apps/user/src/app/app.routing.module.ts
+++ apps/user/src/app/app.routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-<<<<<<< HEAD
   {
     path: '',
     loadChildren: () =>
@@ -10,12 +9,6 @@ const routes: Routes = [
         (m) => m.MainLayoutModule
       ),
   },
-=======
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'history', component: HistoryComponent },
->>>>>>> 9f6a64c... feat(home): chang burger menu in responsive and make routing and change button hover
 ];
 
 @NgModule({
